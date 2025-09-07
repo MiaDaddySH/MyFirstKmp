32efa973bbf4bad69f242ae46cf86761be68489c
Yuangang Sheng <yuangang.sheng@intive.com>
2025-09-07 10:39:46 +0200

    Remove native button in iosApp.

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -11,20 +11,10 @@ struct ComposeView: UIViewControllerRepresentable {
 }
 
 struct ContentView: View {
-    // var body: some View {
-    //     ComposeView()
-    //         .ignoresSafeArea()
-    // }
-    var body: some View {
-        VStack(spacing: 16) {
-            ComposeView() // 现有的 Compose 界面
-            Button("仅 iOS 功能") {
-                // TODO: iOS 专属行为
-            }
-            .buttonStyle(.borderedProminent)
-        }
-        .padding()
-    }
+     var body: some View {
+         ComposeView()
+             .ignoresSafeArea()
+     }
 }
 
 
